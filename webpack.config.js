3903c7f52f0143359c2a635f3e44269df7e38ba2
edo-edo <giorgiediberidze99@gmail.com>
2020-12-03 23:33:38 +0400

    ts loader

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  devServer: {
+    hot: true,
+    port: 3000
+  },
+    entry: "./src/index.tsx",
+    devtool: "inline-source-map",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, 'public')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"]
+    }
+}
