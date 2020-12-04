c3cd13da4a4ab05daca4697e0edc9f4e1bbc643b
edo-edo <ediberidze99@gmail.com>
2020-12-04 04:02:39 +0400

    webpack react with typescript setup

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,6 @@ import * as ReactDOM from 'react-dom';
 
 
 ReactDOM.render(
-  <h1>hello order</h1>,
+  <h1>hello</h1>,
   document.getElementById('root') as HTMLElement
 );
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,63 @@
 const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { HotModuleReplacementPlugin } = require('webpack');
 
 module.exports = {
+  mode: 'development',
   devServer: {
+    host: 'localhost',
+    port: 3000,
     hot: true,
-    port: 3000
+    liveReload: false,
+    historyApiFallback: true,
   },
-    entry: "./src/index.tsx",
+  entry: path.join(__dirname, '/src/index.tsx'),
+
     devtool: "inline-source-map",
     output: {
         filename: "bundle.js",
-        path: path.resolve(__dirname, 'public')
+        path: path.resolve(__dirname, 'dist')
     },
     module: {
         rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            }
+          {
+            test: /\.tsx?$/,
+            use: [
+              {
+                loader: 'babel-loader'
+              }
+            ]
+          },
+          {
+            enforce: 'pre',
+            test: /\.js$/,
+            use: [
+              {
+                loader: 'source-map-loader'
+              }
+            ]
+            
+          },
+          {
+            test: /\.scss$/,
+            use: [
+              {
+                loader: 'style-loader!css-loader!sass-loader',
+              }
+            ]
+            
+          },
         ]
     },
     resolve: {
-        extensions: [".ts", ".tsx", ".js"]
-    }
+        extensions: [".ts", ".tsx", ".js", ".jsx"]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "src", "index.html"),
+        filename: 'index.html',
+        inject: true,
+      }),
+      new HotModuleReplacementPlugin()
+    ],
 }
