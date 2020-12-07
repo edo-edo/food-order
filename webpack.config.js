e9bddc0abe2db1eede8274fdf95426fe6f3444d4
edo-edo <ediberidze99@gmail.com>
2020-12-07 23:23:10 +0400

    add css module type

--- webpack.config.js
+++ webpack.config.js
@@ -39,6 +39,21 @@ module.exports = {
             
           },
           {
+            test: /\.css$/,
+            use: [
+              {
+                loader: "style-loader"
+              },
+              {
+                loader: "css-loader",
+                options: {
+                  importLoaders: 1,
+                  modules: true
+                }
+              }
+            ]
+          },
+          {
             test: /\.scss$/,
             use: [
               {
